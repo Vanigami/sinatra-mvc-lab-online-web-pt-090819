5ee7f8c4af1c5bbf12eca2c1a6dc862ca7c03c76
Vanna Winland <vgwinland@gmail.com>
2019-12-17 00:49:25 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,7 @@ class PigLatinizer
   def piglatinize(input_str)
     input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
-  
+
   def vowel?(text)
     text.match(/[aeiouAEIOU]/)
   end
