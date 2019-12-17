0f6fdd7d0e5d5b161b18e2f00b73b258180eb128
Vanna Winland <vgwinland@gmail.com>
2019-12-17 00:49:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     pl = Piglatinizer.new
-    @word = pl.piglatinize(params[:user_phrase])
+    @piglatin = pl.piglatinize(params[:user_phrase])
     erb :results
   end
 
