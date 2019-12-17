c451ba4725d284ea240f855549b9510aecb6b480
Vanna Winland <vgwinland@gmail.com>
2019-12-17 00:37:10 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,28 @@
+class PigLatinizer
+  attr_reader :input_str
+
+  def piglatinize(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+  end
+
+  def vowel?(text)
+    text.match(/[aeiouAEIOU]/)
+  end
+
+  def piglatinize_word(word)
+    if vowel?(word[0])
+      word = word + "w"
+    elsif !vowel?(word[0]) && !vowel?(word[1]) && !vowel?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    elsif !vowel?(word[0]) && !vowel?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+end
